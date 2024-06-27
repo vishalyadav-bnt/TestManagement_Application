352a1be4f5f995b5acc75298f90ce172820b2b88
Vishal Yadav <vishal.yadav@bnt.local>
2024-06-27 15:17:20 +0530

    I implement swagger and it is a documantion api which allow devlopers to describe their api structure using open api specification

--- mcqtestapplication/mcqtestapplication/src/main/java/com/example/mcqtestapplication/controller/McqQuestionController.java
+++ mcqtestapplication/mcqtestapplication/src/main/java/com/example/mcqtestapplication/controller/McqQuestionController.java
@@ -1,6 +1,9 @@
 package com.example.mcqtestapplication.controller;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,40 +22,50 @@ import com.example.mcqtestapplication.service.McqQuestionServiceimpl;
 @RestController
 @RequestMapping("/api/questions")
 public class McqQuestionController {
+    private static final  Logger log=LoggerFactory.getLogger(McqQuestionController.class);
  @Autowired
  McqQuestionServiceimpl questionService;
     @PostMapping("/create")
     public ResponseEntity<SuccessResponse> createQuestion(@RequestBody McqQuestionModel question) {
+        log.info("Request recieved for creating...");
         McqQuestionModel createdQuestion = questionService.creaQuestionModel(question);
         SuccessResponse response = new SuccessResponse("Question Store SuccesFully", HttpStatus.CREATED.value(),
                 createdQuestion);
+        log.info("Send Response Successfully");
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping("/all")
     public ResponseEntity<SuccessResponse> getAllQuestions() {
+        log.info("Request for fetch all questions");
         List<McqQuestionModel> questions = questionService.getAllQuestions();
         SuccessResponse response = new SuccessResponse("Data Fetch Succesfully", HttpStatus.OK.value(), questions);
+        log.info("Fetch All questions");
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<SuccessResponse> getQuestionById(@PathVariable("id") int id) {
+        log.info("Request for single question fetch");
         McqQuestionModel question = questionService.getQuestionById(id);
         SuccessResponse response = new SuccessResponse("Data fetch by using id", HttpStatus.FOUND.value(), question);
+        log.info("Question fetch succesfully");
         return new ResponseEntity<>(response, HttpStatus.FOUND);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<SuccessResponse> updateQuestion(@PathVariable("id") int id,
             @RequestBody McqQuestionModel question) {
+        log.info("Request for updating question");
         McqQuestionModel updatedQuestion = questionService.updateQuestion(id, question);
         SuccessResponse response=new SuccessResponse("Data Updated...",HttpStatus.OK.value(),updatedQuestion);
+        log.info("Update Data Succesfully");
         return new ResponseEntity<>(response,HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteQuestion(@PathVariable("id") int id) {
+        log.info("Request for delete data");
         questionService.deleteQuestion(id);
         return ResponseEntity.ok("Data Deleted....");
     }
--- mcqtestapplication/mcqtestapplication/src/main/java/com/example/mcqtestapplication/service/McqQuestionServiceimpl.java
+++ mcqtestapplication/mcqtestapplication/src/main/java/com/example/mcqtestapplication/service/McqQuestionServiceimpl.java
@@ -3,6 +3,8 @@ package com.example.mcqtestapplication.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,49 +16,64 @@ import com.example.mcqtestapplication.repositiory.QuestionRepositiory;
 
 @Service
 public class McqQuestionServiceimpl implements McqQuestionService {
+    private static final Logger log=LoggerFactory.getLogger(McqQuestionServiceimpl.class);
+    
     @Autowired
     QuestionRepositiory questionRepositiory;
 
     @Override
     public McqQuestionModel creaQuestionModel(McqQuestionModel mcqQuestionModel) {
+        log.info("comes in service");
         if (mcqQuestionModel == null || mcqQuestionModel.getQuestion() == null) {
+            log.error("Error Occured");
             throw new QuestionModelIsEmpty("Question model is empty");
         }
         McqQuestionModel stroeqQuestionModel = questionRepositiory.save(mcqQuestionModel);
+        log.info("return from service");
         return stroeqQuestionModel;
     }
 
     @Override
     public List<McqQuestionModel> getAllQuestions() {
+        log.info("coming in service for get all questions");
         List<McqQuestionModel> list = questionRepositiory.findAll();
         if (list.isEmpty()) {
+            log.info("throw excption");
             throw new DataIsNotPresent("Data is not present for fetching");
         }
+        log.info("return succesfully");
         return list;
     }
 
     @Override
     public McqQuestionModel getQuestionById(int id) {
+        log.info("comes in service for fetch single questions");
         Optional<McqQuestionModel> question = questionRepositiory.findById(id);
         if (!question.isPresent()) {
+            log.error("error occured");
             throw new IdNotFound("Id Not Found");
         }
         McqQuestionModel questions = question.get();
+        log.info("return from service with single questions model");
         return questions;
     }
 
     @Override
     public void deleteQuestion(int id) {
+        log.info("comes in service for delete data");
         Optional<McqQuestionModel> question = questionRepositiory.findById(id);
         if (question.isPresent()) {
             questionRepositiory.deleteById(id);
+            log.info("Delete data....");
         } else {
+            log.error("throw Exception");
             throw new IdNotFound("Id Not Found");
         }
     }
 
     @Override
     public McqQuestionModel updateQuestion(int id, McqQuestionModel mcqQuestionModel) {
+        log.info("comimg in service for update data");
         Optional<McqQuestionModel> question = questionRepositiory.findById(id);
         if (question.isPresent()) {
             McqQuestionModel questions = question.get();
@@ -70,8 +87,10 @@ public class McqQuestionServiceimpl implements McqQuestionService {
             questions.setPositiveMark(mcqQuestionModel.getPositiveMark());
             questions.setNagativeMark(mcqQuestionModel.getNagativeMark());
             McqQuestionModel updatedData = questionRepositiory.save(questions);
+            log.info("return updated data");
             return updatedData;
         } else {
+            log.error("Error occured");
             throw new IdNotFound("Id Is Not Present");
         }
     }
